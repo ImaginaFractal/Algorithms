594ea49e2ae4b16e4c7d4dd5a090ca80c3c2a32e
Zhuoran <yuzhuoran13@gmail.com>
2024-08-13 07:07:06 +0800

    Rename to Algorithms

--- module.cpp
+++ module.cpp
@@ -7,12 +7,12 @@
 using namespace Imagina;
 
 constexpr ComponentInfo Components[]{
-	ComponentInfo::Evaluator<Perturbation::PerturbationEvaluator>("PerturbationEvaluator", "Sample Perturbation Evaluator"),
-	ComponentInfo::Evaluator<PTWithCompression::PTWithCompressionEvaluator>("PTWithCompression", "Sample Perturbation With Compression Evaluator"),
-	ComponentInfo::Evaluator<HinfLA::HInfLAEvaluator>("HInfLAEvaluator", "Sample HInf LA Evaluator"),
-	ComponentInfo::Evaluator<MipLA::MipLAEvaluator>("MipLAEvaluator", "Sample Mip LA Evaluator"),
+	ComponentInfo::Evaluator<Perturbation::PerturbationEvaluator>("PerturbationEvaluator", "Perturbation"),
+	ComponentInfo::Evaluator<PTWithCompression::PTWithCompressionEvaluator>("PTWithCompression", "Perturbation With Compression"),
+	ComponentInfo::Evaluator<HinfLA::HInfLAEvaluator>("HInfLAEvaluator", "HInf LA"),
+	ComponentInfo::Evaluator<MipLA::MipLAEvaluator>("MipLAEvaluator", "Mip LA"),
 };
-constexpr ModuleInfo Module("SampleEvaluators", "Sample Evaluators", Components);
+constexpr ModuleInfo Module("Algorithms", "Algorithms", Components);
 
 im_api const Imagina::ModuleInfo *ImGetModuleInfo() {
 	return &Module;
